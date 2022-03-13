7c6182eda25ad978baed8225e1a9a65a057fa3e6
vaibhavdighe1999 <55141573+vaibhavdighe1999@users.noreply.github.com>
2022-03-13 17:57:22 +0530

    Add files via upload

---
+++ ASimulatorSystem/src/conn.java
@@ -0,0 +1,21 @@
+//package ASimulatorSystem;
+import java.sql.*;  
+
+class Conn{
+    Connection c;
+    Statement s;
+    public Conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql://localhost/bms","root",""); 
+            s =c.createStatement(); 
+           
+           
+          
+            
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+   
+}  
